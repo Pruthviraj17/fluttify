8f0899393c9b58a9a06a3b8a3b8e57641cf98bb2
Pruthviraj Haladkar <rajhaladkar9808@gmail.com>
2024-06-27 16:05:06 +0530

    fluttify the spotify like clone

---
+++ client/lib/core/providers/current_song_notifier.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: avoid_public_notifier_properties
+import 'package:client/features/home/model/song_model.dart';
+import 'package:client/features/home/repository/home_local_repository.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'current_song_notifier.g.dart';
+
+@riverpod
+class CurrentSongNotifier extends _$CurrentSongNotifier {
+  AudioPlayer? audioPlayer;
+  bool isPlaying = false;
+  late HomeLocalRepository _homeLocalRepository;
+  @override
+  SongModel? build() {
+    _homeLocalRepository = ref.read(homeLocalRepositoryProvider);
+    return null;
+  }
+
+  void updateSong(SongModel song) async {
+    await audioPlayer?.stop();
+    audioPlayer = AudioPlayer();
+
+    final audioSource = AudioSource.uri(
+      Uri.parse(song.song_url),
+      tag: MediaItem(
+        id: song.id,
+        title: song.song_name,
+        artist: song.artist,
+        artUri: Uri.parse(song.thumbnail_url),
+        
+      ),
+    );
+    await audioPlayer!.setAudioSource(audioSource);
+
+    audioPlayer!.playerStateStream.listen((state) {
+      if (state.processingState == ProcessingState.completed) {
+        audioPlayer!.seek(Duration.zero);
+        audioPlayer!.pause();
+        isPlaying = false;
+
+        this.state = this.state?.copyWith(hex_code: this.state?.hex_code);
+      }
+    });
+    _homeLocalRepository.uploadLocalSong(song);
+    audioPlayer!.play();
+    isPlaying = true;
+    state = song;
+  }
+
+  void playPause() {
+    if (isPlaying) {
+      audioPlayer?.pause();
+    } else {
+      audioPlayer?.play();
+    }
+    isPlaying = !isPlaying;
+    state = state?.copyWith(hex_code: state?.hex_code);
+  }
+
+  void seek(double val) {
+    audioPlayer!.seek(
+      Duration(
+        milliseconds: (val * audioPlayer!.duration!.inMilliseconds).toInt(),
+      ),
+    );
+  }
+}
---
+++ client/lib/core/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:client/core/theme/app_palllete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static OutlineInputBorder _border(Color color) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Pallete.backgroundColor,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(Pallete.borderColor),
+      border: _border(Pallete.borderColor),
+      // errorBorder: _border(Pallete.errorColor),
+      focusedBorder: _border(Pallete.gradient2),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Pallete.backgroundColor,
+    ),
+  );
+}
